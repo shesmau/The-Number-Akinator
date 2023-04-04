bed096cca76b22be9c5dc8e721c5ce3c8045a335
manal03 <118558324+manal03@users.noreply.github.com>
2023-04-04 17:34:41 -0400

    Update guessNumberFunc.js

--- frontend/guessNumberFunc.js
+++ frontend/guessNumberFunc.js
@@ -1,17 +1,35 @@
-let max_number = [Math.floor(Math.random() * 100)+1];
+let max_number = (Math.floor(Math.random() * 100)+1);
 
 function maxNum(){
     let output = document.getElementById("result");
+    let instruct = document.getElementById("instruction");
 
     let input = document.getElementById("userInput").value; 
+    let count = ""; 
+     count =count + input;
     if(input == max_number){
-        output.innerHTML = 'Correct';
+        output.innerHTML = "The correct number was " + max_number;
         max_number = [Math.floor(Math.random() * 100)+1];
+        instruct.innerHTML = "CHEERS! - Iffy"
 
     } else if ( input < max_number){
-        output.innerHTML = "Number is too low. Try again!";
+
+        output.innerHTML = "Your number: " + count;
+       // alert("Try a higher number")
+        instruct.innerHTML = "Try something higher than " + input + ""
     }
     else{
-        output.innerHTML = "Number is too high. Try again!";
+
+        output.innerHTML = "Your number: " + count;
+        //alert("Try a lower number")
+        instruct.innerHTML = "Try something lower than " + input + ""
     };
 };
+
+function giveUp(){
+    let output = document.getElementById("result");
+    let instruct = document.getElementById("instruction");
+
+    output.innerHTML = "The number was: " + max_number;
+
+instruct.innerHTML = "Please try again later! <br/> See you next time - Iffy" }
